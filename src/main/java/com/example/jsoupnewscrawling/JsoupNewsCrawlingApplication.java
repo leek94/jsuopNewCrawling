a75efb7b7225ddeb4471da04222b876676d7754d
leek94 <khwan929@naver.com>
2023-10-30 11:23:56 +0900

    init commit

---
+++ src/main/java/com/example/jsoupnewscrawling/JsoupNewsCrawlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsoupnewscrawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsoupNewsCrawlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsoupNewsCrawlingApplication.class, args);
+    }
+
+}
