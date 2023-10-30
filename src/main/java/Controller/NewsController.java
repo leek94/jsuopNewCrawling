952796a9ba1de609d82d8a4730c225aa4ecf6f76
leek94 <khwan929@naver.com>
2023-10-30 11:29:47 +0900

    feat NewsService 생성

--- src/main/java/Controller/NewsController.java
+++ src/main/java/Controller/NewsController.java
@@ -4,6 +4,7 @@ import entity.News;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import service.NewsService;
 
 import java.util.List;
 
